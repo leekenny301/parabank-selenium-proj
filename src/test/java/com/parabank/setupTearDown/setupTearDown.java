a829f4f7ad4e4e61b8677201179c00f2c72f7eb9
Kenny Lee <ylhkenny108@gmail.com>
2022-06-30 12:22:22 +0800

    Created this file for running codes Before and After tests

--- src/test/java/com/parabank/setupTearDown/setupTearDown.java
+++ src/test/java/com/parabank/setupTearDown/setupTearDown.java
@@ -1,2 +1,28 @@
-package com.parabank.setupTearDown;public class setupTearDown {
+package com.parabank.setupTearDown;
+
+import com.parabank.drivers.chrome;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+
+import java.time.Duration;
+
+public class setupTearDown {
+    WebDriver driver;
+    WebDriverWait wait;
+
+    @BeforeTest
+    public void setup(){
+        driver = chrome.setDriver();
+        driver.manage().window().maximize();
+        wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+    }
+
+    @AfterTest
+    public void tearDown() throws InterruptedException {
+        Thread.sleep(2000);
+        driver.close();
+    }
+
 }
